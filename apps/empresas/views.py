426b01214bba9b4fc99d69e80494cdb57b81365f
Glauco Diogo <glauco.oliveir@gmail.com>
2019-06-23 19:12:55 -0300

    Finalizando o crud empresas parte 3

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,7 +1,19 @@
-from django.views.generic.edit import CreateView
+from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Empresa
 
 
 class EmpresaCreate(CreateView):
+    model = Empresa
+    fields = ['nome']
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+        return HttpResponse('Empresa cadastrada com sucesso!')
+
+class EmpresaEdit(UpdateView):
     model = Empresa
     fields = ['nome']
