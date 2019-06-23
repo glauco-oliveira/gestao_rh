2035dbe4b6652e226ef72f5a27387e4ff20aebee
Glauco Diogo <glauco.oliveir@gmail.com>
2019-06-23 16:10:44 -0300

    Criando crud empresas parte 1

--- apps/core/views.py
+++ apps/core/views.py
@@ -4,4 +4,6 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
-    return render(request, 'core/index.html')
+    data = {}
+    data['usuario'] = request.user
+    return render(request, 'core/index.html', data)
